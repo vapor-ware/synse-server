aee5cdfb58e84761a647c9a4c4baa501c10b51fd
Andrew Cencini <andrew@vapor.io>
2015-08-20 10:29:42 -0400

    un-daemonize uwsgi, and output uwsgi logs via start_opendcre.sh
    Signed-off-by: Andrew Cencini <andrew@vapor.io>

--- start_opendcre.sh
+++ start_opendcre.sh
@@ -2,7 +2,7 @@
 
 service nginx start
 if [ ! -z ${1} ]; then
-	uwsgi /opendcre/opendcre_uwsgi.ini --pyargv $1
+	uwsgi /opendcre/opendcre_uwsgi.ini --pyargv $1 >> /logs/opendcre_daemon.log 2>&1
 else
-	uwsgi /opendcre/opendcre_uwsgi.ini
+	uwsgi /opendcre/opendcre_uwsgi.ini >> /logs/opendcre_daemon.log 2>&1
 fi
