b4c47486a01810aac79b23b87b53843871a0a9b4
Kontazler <Kontazler@gmail.com>
2017-05-03 21:19:46 -0500

    Introduce new container layer for custom configuration

--- configure_nginx_config.sh
+++ configure_nginx_config.sh
@@ -1,6 +1,24 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# ---------------------------------------------------------------------
+# Copyright (C) 2015-17  Vapor IO
+#
+# This file is part of Synse.
+#
+# Synse is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Synse is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Synse.  If not, see <http://www.gnu.org/licenses/>.
+# ---------------------------------------------------------------------
 
 VERSION_SENTINEL="SYNSE_VERSION"
-SYNSE_VERSION="1.3"
+SYNSE_VERSION=`python /synse/synse/version.py`
 
 sed -i -e "s/$VERSION_SENTINEL/$SYNSE_VERSION/g" synse_nginx.conf
