8729fd25257359fa62e343b466ccfd3635941f5c
Erick Daniszewski <edaniszewski@gmail.com>
2017-08-04 09:25:52 -0400

    updates logging for the synse application. it will no longer be logged to file in /logs, but will instead go through stdout and stderr, so it can be viewable via docker logs.

--- synse/vapor_common/vapor_logging.py
+++ synse/vapor_common/vapor_logging.py
@@ -46,37 +46,3 @@ def setup_logging(default_path='logging.json', default_level=logging.INFO, env_k
         logging.config.dictConfig(config)
     else:
         logging.basicConfig(level=default_level)
-
-
-def _init_startup_logger():
-    """ Convenience method to initialize the setup error logger.
-    """
-    if not os.path.exists('/logs/err'):
-        # as a fallback to prevent failures, we will just return a plain old logger if the
-        # container isn't configured internally for the setup logger.
-        return logging.getLogger(__name__)
-
-    formatter = logging.Formatter(
-        '[CONTAINER STARTUP ERROR] (%(asctime)s - %(module)s:%(lineno)s): %(message)s')
-    handler = logging.FileHandler('/logs/err')
-    handler.setLevel(logging.DEBUG)
-    handler.setFormatter(formatter)
-
-    logger = logging.getLogger('startup_error')
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(handler)
-
-    global _startup_logger
-    _startup_logger = logger
-
-
-def get_startup_logger():
-    """ Get the startup logger, used to log out to the Container's stdout.
-
-    Returns:
-        Logger: the logger configured to write to the container's stdout,
-            assuming other service-specific configurations allow for it.
-    """
-    if _startup_logger is None:
-        _init_startup_logger()
-    return _startup_logger
