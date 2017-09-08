fdd525ab02b8fe7e504a6f375edba0767a7182cd
MatthewHink <matthew_hink@yahoo.com>
2017-09-08 09:17:49 -0700

    OpenDCRE => Synse. Partial.

--- synse/vapor_common/tests/utils/strings.py
+++ synse/vapor_common/tests/utils/strings.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python
-""" OpenDCRE Test class to avoid hardcoded strings.
+""" Synse Test class to avoid hardcoded strings.
 
     \\//
      \/apor IO
+     
+-------------------------------
+Copyright (C) 2015-17  Vapor IO
+
+This file is part of Synse.
+
+Synse is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Synse is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Synse.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 
@@ -23,13 +41,13 @@ class _S(object):
     DEVICE_TYPE_CURRENT = 'current'
     DEVICE_TYPE_FAN = 'fan'              # TODO: Pretty sure that this is wrong. Should be fan_speed. (552)
     DEVICE_TYPE_FAN_SPEED = 'fan_speed'
-    DEVICE_TYPE_FREQUENCY = 'frequency'  # Device is not currently supported by OpenDCRE.
-    DEVICE_TYPE_HUMIDITY = 'humidity'    # Device is not currently supported by OpenDCRE AFAIK.
+    DEVICE_TYPE_FREQUENCY = 'frequency'  # Device is not currently supported by Synse.
+    DEVICE_TYPE_HUMIDITY = 'humidity'
     DEVICE_TYPE_LEAKAGE = 'leakage'
     DEVICE_TYPE_LED = 'led'
     DEVICE_TYPE_PERCENT_LOAD = 'percent_load'
     DEVICE_TYPE_POWER = 'power'
-    DEVICE_TYPE_PRESSURE = 'pressure'    # Device is not currently supported by OpenDCRE AFAIK.
+    DEVICE_TYPE_PRESSURE = 'pressure'
     DEVICE_TYPE_TEMPERATURE = 'temperature'
     DEVICE_TYPE_VOLTAGE = 'voltage'
     DEVICES = 'devices'
