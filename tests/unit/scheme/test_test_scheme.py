5297a4b1533ad8df539f9c751f3515009248585c
Erick Daniszewski <edaniszewski@gmail.com>
2017-11-13 16:15:05 -0500

    .

--- tests/unit/scheme/test_test_scheme.py
+++ tests/unit/scheme/test_test_scheme.py
@@ -1,13 +1,13 @@
 """Test the 'synse.scheme.test' Synse Server module.
 """
 
-from synse.scheme.test import TestResponse
+from synse.scheme.test import TestResponse as TResp
 
 
 def test_test_scheme():
     """Test that the test scheme matches the expected.
     """
-    response_scheme = TestResponse()
+    response_scheme = TResp()
 
     assert 'status' in response_scheme.data
     assert 'timestamp' in response_scheme.data
