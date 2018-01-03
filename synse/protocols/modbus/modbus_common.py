77e0ce9a4fc6dcfc4bfe6ed845bc644d3249367e
MatthewHink <matthew_hink@yahoo.com>
2018-01-03 14:00:11 -0800

    ECblue fan controller.

--- synse/protocols/modbus/modbus_common.py
+++ synse/protocols/modbus/modbus_common.py
@@ -8,8 +8,25 @@
         The gs3 command line tool and the synse device bus use this code.
 """
 
+import logging
+import struct
 from synse.protocols.modbus import dkmodbus  # nopep8
 from synse.protocols.conversions import conversions  # nopep8
+
+logger = logging.getLogger(__name__)
+
+
+def get_fan_direction_ecblue(ser, slave_address):
+    """Production only direction reads from ECblue fan (vapor_fan).
+    :param ser: Serial connection to the fan controller.
+    :param slave_address: The Modbus slave address of the device.
+    :return: The fan direction of forward or reverse.
+    """
+    direction = read_holding_register(ser, slave_address, 1)
+    direction &= 8
+    if direction == 0:
+        return 'forward'
+    return 'reverse'
 
 
 def get_fan_direction_gs3(ser, slave_address):
@@ -37,6 +54,31 @@ def get_fan_frequency_gs3(ser, slave_address):
     return read_holding_register(ser, slave_address, 0x0002)
 
 
+def get_fan_rpm_ecblue(ser, slave_address):
+    """Production only rpm reads from ECblue fan (vapor_fan).
+    :param ser: Serial connection to the ECblue fan controller.
+    :param slave_address: The Modbus slave address of the device.
+    :return: The fan speed in rpm.
+    """
+    # TODO: There may be another register for this.
+    # We are reading off of the Hz setting and there is going to be ramp up/down
+    # time which is not taken into account here.
+    max_rpm = get_fan_max_rpm_ecblue(ser, slave_address)
+    logger.debug('max_rpm: {}'.format(max_rpm))
+
+    # Get the hz setting in Hz * 10.
+    hz_setting = read_holding_register(ser, slave_address, 0x02)
+    logger.debug('raw Hz setting: 0x{:04x}'.format(hz_setting))
+    # Convert from hex to BCD.
+    hz_setting_string = '{:04x}'.format(hz_setting)
+    hz_setting_int = int(hz_setting_string)
+    hz_setting_float = float(hz_setting_int) / 10.0
+    ratio = hz_setting_float / 60.0
+    rpm = int(max_rpm * ratio)
+    logger.debug('rpm: {}'.format(rpm))
+    return rpm
+
+
 def get_fan_rpm_gs3(ser, slave_address):
     """Get the current fan rpm for a gs3 fan controller.
     :param ser: Serial connection to the gs3 fan controller.
@@ -58,6 +100,15 @@ def get_fan_rpm_to_hz_gs3(ser, slave_address, max_rpm):
     return float(base_frequency) / float(max_rpm)
 
 
+def get_fan_max_rpm_ecblue(ser, slave_address):
+    """Get the maximum rpm of the fan motor through the ECblue fan controller.
+    :param ser: Serial connection to the ECblue fan controller.
+    :param slave_address: The Modbus slave address of the device.
+    :returns: The base maximum rpm of the fan motor.
+    """
+    return read_holding_register(ser, slave_address, 0x08)
+
+
 def get_fan_max_rpm_gs3(ser, slave_address):
     """Get the maximum rpm of the fan motor through the gs3 fan controller.
     :param ser: Serial connection to the gs3 fan controller.
@@ -69,7 +120,7 @@ def get_fan_max_rpm_gs3(ser, slave_address):
 
 def read_holding_register(ser, slave_address, register):
     """Read a holding register from an RS485 device.
-    :param ser: Serial connection to the gs3 fan controller.
+    :param ser: Serial connection to the fan controller.
     Contains baud rate, parity, and timeout.
     :param slave_address: The Modbus slave address of the device.
     :param register: The register to read (int).
@@ -77,8 +128,68 @@ def read_holding_register(ser, slave_address, register):
     """
     client = dkmodbus.dkmodbus(ser)
     register_data = client.read_holding_registers(slave_address, register, 1)
-    result = conversions.unpack_word(register_data)
-    print '0x{}'.format(result)
+    logger.debug('register_data: {}, type(register_data): {}'.format(
+        register_data, type(register_data)))
+    result = unpack_register_data(register_data)
+    logger.debug('read_holding_register result: 0x{:x} {}d'.format(result, result))
+    return result
+
+
+def read_input_register(ser, slave_address, register):
+    """Read an input register from an RS485 device.
+    :param ser: Serial connection to the fan controller.
+    Contains baud rate, parity, and timeout.
+    :param slave_address: The Modbus slave address of the device.
+    :param register: The register to read (int).
+    :returns The register reading (int).
+    """
+    client = dkmodbus.dkmodbus(ser)
+    register_data = client.read_input_registers(slave_address, register, 1)
+    logger.debug('input register_data (raw): {} length: {}'.format(
+        register_data, len(register_data)))
+    result = unpack_register_data(register_data)
+    logger.debug('read_input_register result: 0x{}'.format(result))
+    return result
+
+
+def set_fan_rpm_ecblue(ser, slave_address, rpm_setting, max_rpm):
+    """
+    Set the fan speed in rpm on an ECblue fan controller.
+    :param ser: Serial connection to the gs3 fan controller.
+    Contains baud rate, parity, and timeout.
+    :param slave_address: The Modbus slave address of the device.
+    :param rpm_setting: The user supplied rpm setting.
+    :param max_rpm: The max rpm setting for the fan motor.
+    :return: The modbus write result.
+    """
+    client = dkmodbus.dkmodbus(ser)
+
+    logger.debug('Setting fan speed. max_rpm: {}, rpm_setting: {}'.format(
+        max_rpm, rpm_setting))
+
+    percentage_setting = float(rpm_setting) / float(max_rpm)
+    logger.debug('percentage_setting: {}'.format(percentage_setting))
+    fan_setting_decimal = int(percentage_setting * float(600.0))
+    logger.debug('fan_setting_decimal: {}d 0x{:04x}'.format(
+        fan_setting_decimal, fan_setting_decimal))
+
+    fan_setting = (fan_setting_decimal / 100) << 8
+    logger.debug('fan_setting: {}d 0x{}'.format(fan_setting, fan_setting))
+    setting = ((fan_setting_decimal % 100) / 10) << 4
+    fan_setting += setting
+    logger.debug('fan_setting: {}d 0x{}'.format(fan_setting, fan_setting))
+    setting = (fan_setting_decimal % 10)
+    fan_setting += setting
+    logger.debug('fan_setting: {}d 0x{}'.format(fan_setting, fan_setting))
+
+    fan_setting = struct.pack('>H', fan_setting)
+
+    result = client.write_multiple_registers(
+        slave_address,  # Slave address.
+        2,  # Register to write to.
+        1,  # Number of registers to write to.
+        2,  # Number of bytes to write.
+        fan_setting)  # Data to write.
     return result
 
 
@@ -113,3 +224,18 @@ def set_fan_rpm_gs3(ser, slave_address, rpm_setting, max_rpm):
             4,  # Number of bytes to write.
             packed_hz + '\x00\x01')  # Frequency setting in Hz / data # 01 is on, # 00 is off.
     return result
+
+
+def unpack_register_data(register_data):
+    """
+    Unpacks register_data from a byte string to int.
+    :param register_data: The data to unpack.
+    :return: The data as an int.
+    """
+    length = len(register_data)
+    if length == 2:
+        return conversions.unpack_word(register_data)
+    elif length == 1:
+        return conversions.unpack_byte(register_data)
+    else:
+        raise ValueError('Unexpected length {}.'.format(length))
