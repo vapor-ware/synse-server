099df7f8a99e0f2db5ece6c27f1a493b44e57986
Erick Daniszewski <edaniszewski@gmail.com>
2018-02-21 12:14:43 -0500

    fix config file failures, add config logging

--- runserver.py
+++ runserver.py
@@ -2,15 +2,10 @@
 """
 
 from synse.factory import make_app
-from synse import config
 
 
 if __name__ == '__main__':
-
-    config.load()
-
     app = make_app()
-
     app.run(
         host='0.0.0.0',
         port=5000,
