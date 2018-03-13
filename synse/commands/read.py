ca9675b92746ba89559144333b17e7ebddbfb4e7
Erick Daniszewski <edaniszewski@gmail.com>
2018-03-13 16:19:25 -0400

    remove comprehension where not needed

--- synse/commands/read.py
+++ synse/commands/read.py
@@ -37,7 +37,7 @@ async def read(rack, board, device):
 
     try:
         # perform a gRPC read on the device's managing plugin
-        read_data = [r for r in _plugin.client.read(rack, board, device)]
+        read_data = _plugin.client.read(rack, board, device)
     except grpc.RpcError as ex:
         raise errors.FailedReadCommandError(str(ex)) from ex
 
