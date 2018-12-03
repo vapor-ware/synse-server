d0268d16178bbac5f9936ab4a5a1001d38be5db4
Erick Daniszewski <edaniszewski@gmail.com>
2018-12-03 16:16:12 -0500

    update test socket construction

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@
 
 import logging
 import os
+import shutil
+import socket
 
 import pytest
 import yaml
@@ -9,6 +11,31 @@ import yaml
 from synse import config, const, factory
 
 _app = None
+
+
+class SocketManager:
+    """SocketManager is a helper class for creating, managing, and destroying
+    temporary sockets for testing.
+    """
+
+    def __init__(self, base_path):
+        self.base_path = base_path
+        self.socket_dir = os.path.join(self.base_path, 'socks')
+
+        self._old_socket_path = const.SOCKET_DIR
+        os.mkdir(self.socket_dir)
+        const.SOCKET_DIR = self.socket_dir
+
+    def __del__(self):
+        const.SOCKET_DIR = self._old_socket_path
+        shutil.rmtree(self.socket_dir)
+
+    def add(self, name):
+        """Create a new socket in the socket path."""
+        path = os.path.join(self.socket_dir, name)
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        sock.bind(path)
+        return sock, path
 
 
 @pytest.fixture()
@@ -31,6 +58,20 @@ def app(tmpdir):
         config.options.add_config_paths(appdir)
         _app = factory.make_app()
     return _app
+
+
+@pytest.fixture()
+def tmpsocket():
+    """Fixture that can be used to generate and manage test unix socket data.
+
+    This should be used for all tests that need to set up a unix socket for
+    plugin testing. Pytest's tmpdir fixture generally will create temporary
+    directories with long path names, but socket paths are character bound,
+    so that can lead to test errors.
+
+    The socket test data here is all stored in the `tests/testdata` directory.
+    """
+    yield SocketManager('tests/testdata')
 
 
 @pytest.fixture(autouse=True)
