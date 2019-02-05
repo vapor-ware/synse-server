05d13b4ed800f63348cb25868860466f7dae9b6d
Erick Daniszewski <edaniszewski@gmail.com>
2019-02-05 10:10:12 -0500

    add wrapper around application constructor (#266)

--- synse_server/app.py
+++ synse_server/app.py
@@ -2,78 +2,70 @@
 # pylint: disable=unused-variable,unused-argument
 
 import asyncio
-import os
 
 from sanic import Sanic
 from sanic.exceptions import InvalidUsage, NotFound, ServerError
 from sanic.response import text
 
-import synse_server
-from synse_server import config, errors, utils
+from synse_server import errors, utils
 from synse_server.cache import clear_all_meta_caches, configure_cache
 from synse_server.log import logger, setup_logger
 from synse_server.response import json
 from synse_server.routes import aliases, base, core
 
 
-def make_app():
+def new_app():
     """Create a new instance of the Synse Server Sanic application.
 
-    This is the means by which all Synse Server applications should
-    be created.
-
     Returns:
-        Sanic: A Sanic application setup and configured to serve
-            Synse Server routes.
+        Sanic: A Sanic application for Synse Server.
     """
-    #app = Sanic(__name__, log_config=LOGGING)
-    app = Sanic(__name__)
+
+    app = Sanic(
+        name='synse-server'
+    )
+
+    # Disable the default Sanic logo.
     app.config.LOGO = None
 
-    # Get the application configuration(s)
-    config.options.add_config_paths('.', '/synse/config')
-    config.options.env_prefix = 'SYNSE'
-    config.options.auto_env = True
+    # Register the endpoint blueprints with the application.
+    app.blueprint(aliases.bp)
+    app.blueprint(base.bp)
+    app.blueprint(core.bp)
 
-    config.options.parse(requires_cfg=False)
-    config.options.validate()
+    # Disable favicon
+    #
+    # Add a route which provides an empty response for '/favicon.ico' in order
+    # to prevent missing favicon errors from populating the logs when Synse
+    # Server is hit via web browser.
+    # TODO (etd): we could add an icon to return here if we wanted.
+    app.add_route(lambda *_: text(''), '/favicon.ico')
 
-    # Set up application logging
-    setup_logger()
+    # Register the error handler
+    # FIXME (etd): there is probably a better way to do this, will look into
+    #  this in future issue.
+    _register_error_handling(app)
 
     # Set the language environment variable to that set in the config, if
     # it is not already set. This is how we specify the language/locale for
     # the application.
     # FIXME (etd): this isn't a great way of doing things, especially if Synse
     # Server is being run in a non-containerized environment.
-    lang = os.environ.get('LANGUAGE')
-    if lang:
-        logger.info('LANGUAGE set from env: {}'.format(lang))
-    else:
-        lang = config.options.get('locale')
-        logger.info('LANGUAGE set from config: {}'.format(lang))
-        os.environ['LANGUAGE'] = lang
+    # lang = os.environ.get('LANGUAGE')
+    # if lang:
+    #     logger.info('LANGUAGE set from env: {}'.format(lang))
+    # else:
+    #     lang = config.options.get('locale')
+    #     logger.info('LANGUAGE set from config: {}'.format(lang))
+    #     os.environ['LANGUAGE'] = lang
 
-    # Register the blueprints
-    app.blueprint(aliases.bp)
-    app.blueprint(base.bp)
-    app.blueprint(core.bp)
-
-    _disable_favicon(app)
-    _register_error_handling(app)
-
+    # FIXME (etd): this probably belongs in the Synse class
+    setup_logger()
     configure_cache()
 
     # Add background tasks
     app.add_task(periodic_cache_invalidation)
 
-    # Log out metadata for Synse Server and the application configuration
-    logger.info('Synse Server:')
-    logger.info('  version: {}'.format(synse_server.__version__))
-    logger.info('  author:  {}'.format(synse_server.__author__))
-    logger.info('  url:     {}'.format(synse_server.__url__))
-    logger.info('  license: {}'.format(synse_server.__license__))
-    logger.info('Configuration: {}'.format(config.options.config))
     return app
 
 
@@ -93,18 +85,6 @@ async def periodic_cache_invalidation():
                 'will try again in {}s: {}'
                 .format(interval, e)
             )
-
-
-def _disable_favicon(app):
-    """Return empty response when looking for favicon.
-
-    Args:
-        app: The Sanic application to add the route to.
-    """
-    @app.route('/favicon.ico')
-    def favicon(*_):
-        """Return empty response on favicon request."""
-        return text('')
 
 
 def _register_error_handling(app):
