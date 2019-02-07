744d6ca7feff8cc1a06acce86bd0f8e4bf67eeb8
Erick Daniszewski <edaniszewski@gmail.com>
2019-02-07 11:21:00 -0500

    unify sanic log and synse log formats

--- synse_server/log.py
+++ synse_server/log.py
@@ -1,11 +1,56 @@
 """Synse Server application logging."""
 
 import logging
+import logging.config
 import sys
 
 import structlog
 
 from synse_server import config
+
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'loggers': {
+        'root': {
+            'level': 'INFO',
+            'handlers': ['default']
+        },
+        'sanic.error': {
+            'level': 'INFO',
+            'handlers': ['error'],
+            'propagate': True,
+            'qualname': 'sanic.error'
+        },
+        'sanic.access': {
+            'level': 'INFO',
+            'handlers': ['default'],
+            'propagate': True,
+            'qualname': 'sanic.access'
+        },
+        'synse-server': {
+            'level': 'INFO',
+            'handlers': ['default']
+        },
+    },
+    'handlers': {
+        'default': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'json',
+            'stream': sys.stdout
+        },
+        'error': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'json',
+            'stream': sys.stderr
+        },
+    },
+    'formatters': {
+        'json': {
+            'class': 'pythonjsonlogger.jsonlogger.JsonFormatter'
+        },
+    }
+})
 
 structlog.configure(
     processors=[
@@ -16,9 +61,7 @@ structlog.configure(
         structlog.processors.StackInfoRenderer(),
         structlog.processors.format_exc_info,
         structlog.processors.UnicodeDecoder(),
-        structlog.processors.KeyValueRenderer(
-            key_order=['timestamp', 'level', 'event'],
-        ),
+        structlog.stdlib.render_to_log_kwargs,
     ],
     context_class=dict,
     logger_factory=structlog.stdlib.LoggerFactory(),
@@ -26,32 +69,10 @@ structlog.configure(
     cache_logger_on_first_use=True,
 )
 
-
 logger = structlog.get_logger('synse-server')
-
-
-# TODO: could do something like this I guess.. see how this could tie in to structlog though.
-import logging
-
-l = logging.getLogger('sanic.access')
-l.addHandler()
-
-d = logging.getLogger('sanic.error')
-d.addHandler()
 
 
 def setup_logger():
     """Configure the Synse Server logger."""
     level = logging.getLevelName(config.options.get('logging', 'info').upper())
-
-    # TODO: instead of using the mallet that is basicConfig, we should exercise
-    #  more care into how the loggers are defined to:
-    #    a.) prevent duplicate logs via logger propagation
-    #    b.) ensure that logs are all output in a common format.
-    #  this might take a little work (and possibly monkeying) with how Sanic
-    #  does its logging.
-    logging.basicConfig(
-        format='%(message)s',
-        stream=sys.stdout,
-        level=level,
-    )
+    logger.setLevel(level)
