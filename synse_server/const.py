f102631cea941b8e4419b8fa219fa6618e2ebdf4
Erick Daniszewski <edaniszewski@gmail.com>
2019-04-05 16:05:29 -0400

    various incremental updates (#289)

--- synse_server/const.py
+++
@@ -1,5 +0,0 @@
-"""Synse Server application-wide constants."""
-
-# QUESTION (etd) - should this be 'procs', or something else? perhaps 'run' or 'sock'
-# FIXME: we could just use the path defined in server.py and remove this file.
-SOCKET_DIR = '/tmp/synse/procs'
