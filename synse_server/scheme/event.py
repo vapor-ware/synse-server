c2a517090918830ea3e2f2fb4464bb7e2960a879
Erick Daniszewski <edaniszewski@gmail.com>
2019-04-08 14:09:49 -0400

    remove old files

--- synse_server/scheme/event.py
+++
@@ -1,8 +0,0 @@
-
-
-class Event:
-    """"""
-
-    # TODO: implement -- modeling for a websocket event. unclear if this
-    #  should live here or somewhere else, and how it can be composed of
-    #  other scheme responses.
