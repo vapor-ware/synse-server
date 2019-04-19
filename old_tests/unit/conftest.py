a0edf5b6f2003e5b5d9c638e3b0d8e21b92c6e7d
Erick Daniszewski <edaniszewski@gmail.com>
2019-04-18 23:02:46 -0400

    temporarily move existing tests for clean slate start

---
+++ old_tests/unit/conftest.py
@@ -0,0 +1,40 @@
+"""Test configuration for Synse Server unit tests."""
+
+import os
+
+import bison
+import pytest
+
+from synse_server import cache, config, const, plugin
+
+
+@pytest.fixture(autouse=True)
+def reset_state():
+    """Fixture to reset all Synse Server state between tests."""
+
+    _old = const.SOCKET_DIR
+
+    yield
+
+    # reset configuration
+    config.options = bison.Bison(config.scheme)
+    config.options.env_prefix = 'SYNSE'
+    config.options.auto_env = True
+
+    # reset managed plugins
+    plugin.Plugin.manager.plugins = {}
+
+    # clear the environment
+    for k, _ in os.environ.items():
+        if k.startswith('SYNSE_'):
+            del os.environ[k]
+
+    # reset the socket directory
+    const.SOCKET_DIR = _old
+
+
+@pytest.fixture()
+async def clear_caches():
+    """Fixture to clear all caches before a test starts."""
+    await cache.clear_all_meta_caches()
+    await cache.clear_cache(cache.NS_TRANSACTION)
