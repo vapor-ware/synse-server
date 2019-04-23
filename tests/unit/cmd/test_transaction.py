6bd7a4824ea2045ee88a84e38b4636ed57e7b26e
Erick Daniszewski <edaniszewski@gmail.com>
2019-04-23 07:43:44 -0400

    add stubs for additional tests

--- tests/unit/cmd/test_transaction.py
+++ tests/unit/cmd/test_transaction.py
@@ -20,5 +20,30 @@ async def test_transaction_plugin_not_found():
 
 
 @pytest.mark.asyncio
-async def test_transaction_():
+async def test_transaction_client_unexpected_error():
+    pass
+
+
+@pytest.mark.asyncio
+async def test_transaction_client_expected_error():
+    pass
+
+
+@pytest.mark.asyncio
+async def test_transaction_client_ok():
+    pass
+
+
+@pytest.mark.asyncio
+async def test_transactions_none_cached():
+    pass
+
+
+@pytest.mark.asyncio
+async def test_transactions_one_cached():
+    pass
+
+
+@pytest.mark.asyncio
+async def test_transactions_multiple_cached():
     pass
