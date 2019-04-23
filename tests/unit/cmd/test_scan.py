064ee760b5367d125629e31be77f26b90c429c54
Erick Daniszewski <edaniszewski@gmail.com>
2019-04-23 14:06:18 -0400

    add tests for scan command

--- tests/unit/cmd/test_scan.py
+++ tests/unit/cmd/test_scan.py
@@ -1,34 +1,169 @@
 
+import asynctest
 import pytest
+from synse_grpc import api
 
+from synse_server import errors
 from synse_server.cmd import scan
 
 
 @pytest.mark.asyncio
 async def test_scan_force_error():
-    pass
+    with asynctest.patch('synse_server.cache.update_device_cache') as mock_update:
+        mock_update.side_effect = ValueError()
+
+        with pytest.raises(errors.ServerError):
+            await scan.scan('default', [], '', force=True)
+
+        mock_update.assert_called_once()
 
 
 @pytest.mark.asyncio
-async def test_scan_no_tags():
-    pass
+async def test_scan_no_devices():
+    with asynctest.patch('synse_server.cache.update_device_cache') as mock_update:
+        with asynctest.patch('synse_server.cache.get_devices') as mock_get:
+            mock_get.return_value = []
+
+            resp = await scan.scan('default', ['foo'], '', force=False)
+            assert len(resp) == 0
+
+        mock_update.assert_not_called()
+        mock_get.assert_called_once()
+        mock_get.assert_called_with('default/foo')
 
 
 @pytest.mark.asyncio
 async def test_scan_get_devices_errors():
-    pass
+    with asynctest.patch('synse_server.cache.update_device_cache') as mock_update:
+        with asynctest.patch('synse_server.cache.get_devices') as mock_get:
+            mock_get.side_effect = ValueError()
+
+            with pytest.raises(errors.ServerError):
+                await scan.scan('default', ['foo', 'test/bar'], '', force=False)
+
+        mock_update.assert_not_called()
+        mock_get.assert_called_once()
+        mock_get.assert_called_with('default/foo', 'test/bar')
+
+
+@pytest.mark.asyncio
+async def test_scan_invalid_keys():
+    with asynctest.patch('synse_server.cache.update_device_cache') as mock_update:
+        with asynctest.patch('synse_server.cache.get_devices') as mock_get:
+            mock_get.return_value = [
+                api.V3Device(
+                    id='1',
+                    type='foo',
+                    plugin='abc',
+                    tags=[
+                        api.V3Tag(namespace='default', label='foo')
+                    ]
+                ),
+                api.V3Device(
+                    id='2',
+                    type='foo',
+                    plugin='def',
+                    tags=[
+                        api.V3Tag(namespace='default', label='foo')
+                    ]
+                ),
+                api.V3Device(
+                    id='3',
+                    type='bar',
+                    plugin='abc',
+                    tags=[
+                        api.V3Tag(namespace='default', label='foo')
+                    ]
+                ),
+            ]
+
+            with pytest.raises(errors.InvalidUsage):
+                await scan.scan('default', ['foo'], 'not-a-key,tags', force=False)
+
+        mock_update.assert_not_called()
+        mock_get.assert_called_once()
+        mock_get.assert_called_with('default/foo')
 
 
 @pytest.mark.asyncio
 async def test_scan_ok():
-    pass
+    with asynctest.patch('synse_server.cache.update_device_cache') as mock_update:
+        with asynctest.patch('synse_server.cache.get_devices') as mock_get:
+            mock_get.return_value = [
+                api.V3Device(
+                    id='1',
+                    type='foo',
+                    plugin='abc',
+                    tags=[
+                        api.V3Tag(namespace='default', label='foo')
+                    ]
+                ),
+                api.V3Device(
+                    id='2',
+                    type='foo',
+                    plugin='def',
+                    tags=[
+                        api.V3Tag(namespace='default', label='foo')
+                    ]
+                ),
+                api.V3Device(
+                    id='3',
+                    type='bar',
+                    plugin='abc',
+                    tags=[
+                        api.V3Tag(namespace='default', label='foo')
+                    ]
+                ),
+            ]
+
+            resp = await scan.scan('default', ['foo'], 'plugin,sortIndex,id', force=True)
+            assert len(resp) == 3
+            assert resp[0]['id'] == '1'
+            assert resp[1]['id'] == '3'
+            assert resp[2]['id'] == '2'
+
+        mock_update.assert_called_once()
+        mock_get.assert_called_once()
+        mock_get.assert_called_with('default/foo')
 
 
 @pytest.mark.asyncio
-async def test_scan_ok_tags_no_namespace():
-    pass
+async def test_scan_sort_ok():
+    with asynctest.patch('synse_server.cache.update_device_cache') as mock_update:
+        with asynctest.patch('synse_server.cache.get_devices') as mock_get:
+            mock_get.return_value = [
+                api.V3Device(
+                    id='1',
+                    type='foo',
+                    plugin='abc',
+                    tags=[
+                        api.V3Tag(namespace='default', label='foo')
+                    ]
+                ),
+                api.V3Device(
+                    id='2',
+                    type='foo',
+                    plugin='def',
+                    tags=[
+                        api.V3Tag(namespace='default', label='foo')
+                    ]
+                ),
+                api.V3Device(
+                    id='3',
+                    type='bar',
+                    plugin='abc',
+                    tags=[
+                        api.V3Tag(namespace='default', label='foo')
+                    ]
+                ),
+            ]
 
+            resp = await scan.scan('default', ['foo'], 'type,plugin,id', force=True)
+            assert len(resp) == 3
+            assert resp[0]['id'] == '3'
+            assert resp[1]['id'] == '1'
+            assert resp[2]['id'] == '2'
 
-@pytest.mark.asyncio
-async def test_scan_force_ok():
-    pass
+        mock_update.assert_called_once()
+        mock_get.assert_called_once()
+        mock_get.assert_called_with('default/foo')
