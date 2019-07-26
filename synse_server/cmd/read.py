5f8dd7cb78444585c53ab347933e72f51b32f8c8
Erick Daniszewski <edaniszewski@gmail.com>
2019-07-26 15:04:33 -0400

    websocket streamed readings (#322)
    * initial work for streamed readings poc
    
    * poc work
    
    * some progress.. I think
    
    * proper client cancellation handling
    
    * cleanup
    
    * streamed readings should not block

--- synse_server/cmd/read.py
+++ synse_server/cmd/read.py
@@ -1,4 +1,8 @@
 
+import asyncio
+import queue
+import threading
+
 import synse_grpc.utils
 
 from synse_server import cache, errors, plugin
@@ -138,3 +142,122 @@ async def read_cache(start=None, end=None):
             raise errors.ServerError(
                 _('error while issuing gRPC request: read cache'),
             ) from e
+
+
+class Stream(threading.Thread):
+    """A thread which streams reading data from a plugin.
+
+    The reading data received from the plugin is put onto the thread-safe
+    queue specified on initialization.
+
+    Args:
+        plugin (Plugin): The plugin to gather reading data from.
+        ids (list[str]): A list of device IDs which can be used to constrain
+            the devices for which readings should be streamed.
+        tag_groups (Iterable[list[string]]): A collection of tag groups to
+            constrain the devices for which readings should be streamed. The
+            tags within a group are subtractive (e.g. a device must match all
+            tags in the group to match the filter), but each tag group specified
+            is additive (e.g. readings will be streamed for the union of all
+            specified groups).
+        q (queue.Queue): The thread-safe queue to pass collected readings to.
+    """
+
+    def __init__(self, plugin, ids, tag_groups, q):
+        super(Stream, self).__init__()
+        self.plugin = plugin
+        self.q = q
+        self.ids = ids
+        self.tag_groups = tag_groups
+        self.event = threading.Event()
+
+    def run(self):
+        """Run the thread."""
+        try:
+            with self.plugin as client:
+                for reading in client.read_stream(devices=self.ids, tag_groups=self.tag_groups):
+                    self.q.put(reading_to_dict(reading))
+
+                    # Important: we need to check if the thread event is set -- this
+                    # allows the thread to be cancellable.
+                    if self.event.is_set():
+                        logger.info(_('stream thread cancelled'), plugin=self.plugin.id)
+                        break
+
+        except Exception as e:
+            raise errors.ServerError(
+                _('error while issuing gRPC request: read stream'),
+            ) from e
+
+    def cancel(self):
+        """Cancel the thread."""
+        logger.info(_('cancelling reading stream'), plugin=self.plugin.id)
+        self.event.set()
+
+
+async def read_stream(ws, ids=None, tag_groups=None):
+    """Stream reading data from registered plugins for the provided websocket.
+
+    Note that this will only work for the Synse WebSocket API as of v3.0.
+
+    Args:
+        ws (websockets.WebSocketCommonProtocol): The WebSocket for the request.
+            Note that this command only works with the WebSocket API as of v3.0
+        ids (list[str]): A list of device IDs which can be used to constrain
+            the devices for which readings should be streamed. If no IDs are
+            specified, no filtering by ID is done.
+        tag_groups (Iterable[list[string]]): A collection of tag groups to
+            constrain the devices for which readings should be streamed. The
+            tags within a group are subtractive (e.g. a device must match all
+            tags in the group to match the filter), but each tag group specified
+            is additive (e.g. readings will be streamed for the union of all
+            specified groups). If no tag groups are specified, no filtering by
+            tags is done.
+
+    Yields:
+        dict: The device reading, formatted as a Python dictionary.
+    """
+
+    logger.debug(_('issuing command'), command='READ STREAM', ids=ids, tag_groups=tag_groups)
+
+    q = queue.Queue()
+
+    threads = []
+    for p in plugin.manager:
+        t = Stream(p, ids, tag_groups, q)
+        t.start()
+        threads.append(t)
+
+    def close_callback(*args, **kwargs):
+        logger.debug(_('executing callback to cancel read stream threads'))
+        for stream in threads:
+            stream.cancel()
+
+    # The websocket has a 'close_connection_task' which will run once the
+    # data transfer task as completed or been cancelled. This task should
+    # always be run in the lifecycle of the websocket. We attach a callback
+    # to the task to terminate the stream threads associated with the request,
+    # therefore terminating the synse-server<->plugin(s) stream when the
+    # client<->synse-server websocket is closed.
+    ws.close_connection_task.add_done_callback(close_callback)
+
+    logger.debug(_('collecting streamed readings...'))
+    try:
+        while True:
+            # Needed as an async break point in the loop, particularly for
+            # cancelling the async task.
+            await asyncio.sleep(0)
+            try:
+                val = q.get_nowait() or None
+            except queue.Empty:
+                await asyncio.sleep(0.25)
+                continue
+            else:
+                if val is not None:
+                    yield val
+    finally:
+        # The above should run until either the task is cancelled or there is
+        # an exception. In either case, make the threads are terminated prior
+        # to returning from this function so we are not constantly streaming
+        # readings in the background.
+        close_callback()
