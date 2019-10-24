de580211a14fc900b9c81ed3596a3ca930713cb2
Erick Daniszewski <edaniszewski@gmail.com>
2019-10-24 16:59:38 -0400

    add response bytes counter

--- synse_server/metrics.py
+++ synse_server/metrics.py
@@ -1,10 +1,11 @@
 """Application metrics for Synse Server."""
 
-import sanic
-from prometheus_client import CONTENT_TYPE_LATEST, core, Counter, Histogram
-from prometheus_client.exposition import generate_latest
-from sanic.response import raw
 import time
+
+import sanic
+from prometheus_client import Counter, Histogram, core
+from prometheus_client.exposition import CONTENT_TYPE_LATEST, generate_latest
+from sanic.response import raw
 
 
 class Monitor:
@@ -12,15 +13,21 @@ class Monitor:
     _req_start_time = '__req_start_time'
 
     # Counter for the total number of requests received by Sanic.
-    req_count = Counter(
-        name='sanic_request_count',
-        documentation='Sanic Request Count',
+    http_req_count = Counter(
+        name='sanic_http_request_count',
+        documentation='Sanic HTTP Request Count',
         labelnames=('method', 'endpoint', 'http_code'),
     )
 
-    req_latency = Histogram(
-        name='sanic_request_latency_sec',
-        documentation='Sanic Request Latency',
+    http_req_latency = Histogram(
+        name='sanic_http_request_latency_sec',
+        documentation='Sanic HTTP Request Latency',
+        labelnames=('method', 'endpoint', 'http_code'),
+    )
+
+    http_resp_bytes = Counter(
+        name='sanic_http_response_bytes',
+        documentation='Sanic HTTP Response Bytes',
         labelnames=('method', 'endpoint', 'http_code'),
     )
 
@@ -34,10 +41,6 @@ class Monitor:
         collectors.
         """
 
-        # @self.app.listener('before_server_start')
-        # def before_server(app, loop):
-        #     pass
-
         @self.app.middleware('request')
         async def before_request(request):
             request[self._req_start_time] = time.time()
@@ -49,12 +52,18 @@ class Monitor:
             # WebSocket handler ignores response logic, so default
             # to a 200 response in such case.
             code = response.status if response else 200
+            labels = (request.method, request.path, code)
 
-            self.req_latency.labels(request.method, request.path, code).observe(latency)
-            self.req_count.labels(request.method, request.path, code).inc()
+            self.http_req_latency.labels(*labels).observe(latency)
+            self.http_req_count.labels(*labels).inc()
+
+            # We cannot use Content-Length header since that has not yet been
+            # calculated and added to the response headers.
+            if response.body is not None:
+                self.http_resp_bytes.labels(*labels).inc(len(response.body))
 
         @self.app.route('/metrics', methods=['GET'])
-        async def metrics(request):
+        async def metrics(_):
             return raw(
                 generate_latest(core.REGISTRY),
                 content_type=CONTENT_TYPE_LATEST,
