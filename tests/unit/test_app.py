77973c09ba5785a9d679a9ef2854ef5930c0dc21
Erick Daniszewski <edaniszewski@gmail.com>
2020-06-24 12:18:28 -0400

    bugfix: fix middleware logger to not get body length for streamed responses

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -1,5 +1,8 @@
 """Unit tests for the ``synse_server.app`` module."""
 
+from sanic.response import HTTPResponse, StreamingHTTPResponse
+from structlog import contextvars
+
 from synse_server import app, errors
 
 
@@ -14,3 +17,43 @@ def test_new_app():
     assert 'core-http' in synse_app.blueprints
     assert 'v3-http' in synse_app.blueprints
     assert 'v3-websocket' in synse_app.blueprints
+
+
+def test_on_response_normal_http_response():
+    class MockRequest:
+        method = 'GET'
+        url = 'http://localhost'
+
+    req = MockRequest()
+    resp = HTTPResponse()
+
+    contextvars.bind_contextvars(
+        request_id='test-request',
+    )
+    ctx = contextvars._get_context()
+    assert 'test-request' == ctx.get('request_id')
+
+    app.on_response(req, resp)
+
+    ctx = contextvars._get_context()
+    assert ctx.get('request_id') is None
+
+
+def test_on_response_streaming_http_response():
+    class MockRequest:
+        method = 'GET'
+        url = 'http://localhost'
+
+    req = MockRequest()
+    resp = StreamingHTTPResponse(None)
+
+    contextvars.bind_contextvars(
+        request_id='test-request',
+    )
+    ctx = contextvars._get_context()
+    assert 'test-request' == ctx.get('request_id')
+
+    app.on_response(req, resp)
+
+    ctx = contextvars._get_context()
+    assert ctx.get('request_id') is None
